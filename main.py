7edcce680e956522134121d922f5c8712e6ef905
IrinaMandarina <irina.e.tarasova@gmail.com>
2020-11-18 17:10:57 +0300

    В принципе как-то реализовано взаимодействие спрайтов (косячно очень), движение этого чубрика. Есть второй нарисованный чубрик, его нужно будет менять с первым. Чубрикам можно дорисовать оружие, которым они будут махать.

--- main.py
+++ main.py
@@ -3,16 +3,54 @@ from pygame.draw import *
 
 pygame.init()
 
+g = 0
+
+
+class Hero(pygame.sprite.Sprite):
+
+    def __init__(self, screen, x, y, dx, dy):
+        pygame.sprite.Sprite.__init__(self)
+        self.x = x
+        self.dx = dx
+        self.y = y
+        self.dy = dy + g
+        self.screen = screen
+        self.m = 1
+        self.rect = pygame.Rect(self.x + self.dx, self.y + self.dy, 79, 100)
+
+    def draw(self):
+        image1 = pygame.image.load('hero1.png')
+        image1 = pygame.transform.scale(image1, (79, 100))
+        if self.m == -1:
+            image1 = pygame.transform.flip(image1, True, False)
+        image1.set_colorkey((255, 255, 255))
+        screen.blit(image1, (self.x, self.y))
+        self.rect = pygame.Rect(self.x + self.dx, self.y + self.dy, 79, 100)
+
+
+def move(hero, platforms):
+    if hero.x < 0 or hero.x + 79 > 1000:
+        hero.dx *= (-1)
+        hero.m *= (-1)
+    for platform in platforms:
+        if not pygame.sprite.collide_rect(hero, platform):
+            hero.y = hero.y + hero.dy
+    hero.x = hero.x + hero.dx
+
 
 class Platform(pygame.sprite.Sprite):
+
     def __init__(self, screen, x, y, l):
+        pygame.sprite.Sprite.__init__(self)
         self.x = x
         self.y = y
         self.l = l
         self.screen = screen
+        self.rect = pygame.Rect(self.x, self.y, self.l, 15)
 
     def draw(self):
-        rect(self.screen, (194, 120, 16), (self.x, self.y - 15, self.l, 30))
+        rect(self.screen, (194, 120, 16), (self.x, self.y - 15, self.l, 30), border_bottom_left_radius=14,
+             border_bottom_right_radius=14)
         points = [(self.x, self.y - 15), (self.x, self.y)]
         k = 1
         for i in range(10, self.l, 10):
@@ -29,19 +67,23 @@ screen = pygame.display.set_mode((1000, 700))
 pygame.display.update()
 clock = pygame.time.Clock()
 finished = False
+platforms = []
+hero = Hero(screen, 10, 10, 4, 1)
 
 while not finished:
     clock.tick(FPS)
 
     screen.fill((255, 255, 255))
     image = pygame.image.load('forest1.jpg')
-    image.set_colorkey((255, 255, 255))
     image = pygame.transform.scale(image, (1000, 600))
     image.set_alpha(200)
     screen.blit(image, (0, 100))
 
     pl = Platform(screen, 0, 250, 500)
+    platforms.append(pl)
+    move(hero, platforms)
     pl.draw()
+    hero.draw()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
