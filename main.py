9a6104c0f9d242f2850b69acf94709f4a90fb793
IrinaMandarina <irina.e.tarasova@gmail.com>
2020-11-20 12:14:40 +0300

    Движение контролируется стрелочками.

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ g = 1
 
 class Hero(pygame.sprite.Sprite):
 
-    def __init__(self, screen, x, y, dx, dy):
+    def __init__(self, screen, x, y, dx=0, dy=0):
         pygame.sprite.Sprite.__init__(self)
         self.x = x
         self.dx = dx
@@ -26,26 +26,6 @@ class Hero(pygame.sprite.Sprite):
         image1.set_colorkey((255, 255, 255))
         screen.blit(image1, (self.x, self.y))
         self.rect = pygame.Rect(self.x, self.y, 79, 100)
-
-
-def move(hero, platforms):
-    k = False
-    if hero.x < 0 or hero.x + 79 > 1000:
-        hero.dx *= (-1)
-        hero.m *= (-1)
-    for platform in platforms:
-        if pygame.sprite.collide_rect(hero, platform):
-            k = True
-            hero.dy = 0
-            hero.y = platform.y - 115
-    if hero.y + 100 > 700:
-        hero.y = 600
-        k = True
-    if not k and hero.y < 600:
-        hero.y += hero.dy
-        hero.dy += g
-    hero.x += hero.dx
-
 
 class Platform(pygame.sprite.Sprite):
 
@@ -70,6 +50,24 @@ class Platform(pygame.sprite.Sprite):
         polygon(self.screen, (80, 180, 89), points)
 
 
+def move(hero, platforms):
+    k = False
+    if hero.x < 0 or hero.x + 79 > 1000:
+        hero.dx *= (-1)
+        hero.m *= (-1)
+    for platform in platforms:
+        if pygame.sprite.collide_rect(hero, platform):
+            k = True
+            hero.dy = 0
+            hero.y = platform.y - 115
+    if hero.y + 100 > 700:
+        hero.y = 600
+        k = True
+    if not k and hero.y < 600:
+        hero.y += hero.dy
+        hero.dy += g
+    hero.x += hero.dx
+
 FPS = 20
 screen = pygame.display.set_mode((1000, 700))
 
@@ -77,7 +75,9 @@ pygame.display.update()
 clock = pygame.time.Clock()
 finished = False
 platforms = []
-hero = Hero(screen, 10, 10, 4, 4)
+timer = 0
+
+hero = Hero(screen, 0, 35)
 
 while not finished:
     clock.tick(FPS)
@@ -88,16 +88,30 @@ while not finished:
     image.set_alpha(200)
     screen.blit(image, (0, 100))
 
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            finished = True
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                hero.dx = -1
+                hero.m = (-1)
+            if event.key == pygame.K_RIGHT:
+                hero.dx = 1
+                hero.m = 1
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT:
+                hero.dx = 0
+            if event.key == pygame.K_RIGHT:
+                hero.dx = 0
+
+
     pl = Platform(screen, 0, 250, 500)
     platforms.append(pl)
     move(hero, platforms)
     pl.draw()
     hero.draw()
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            finished = True
-        # if event.type == pygame.MOUSEBUTTONDOWN:
+    timer += 1
 
     pygame.display.update()
 
